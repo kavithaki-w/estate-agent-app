2c88aee5136e1e05b6766d8db450b8f7e90e8fee
Kavithaki Wijesekara <kavithakiw@gmail.com>
2026-01-04 00:15:30 +0530

    Added a folder containing images to public folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import {createBrowserRouter} from "react-router-dom"
+
+const router = createBrowserRouter([])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
