f0b36949bf2decee9753cf84eeb2b9a4fe84d544
Kavithaki Wijesekara <kavithakiw@gmail.com>
2026-01-03 15:50:49 +0530

    Created Favourites component and Results page

---
+++ src/ResultsPage.jsx
@@ -0,0 +1,34 @@
+import {useState} from "react"
+import PropertyCardLarge from "./components/PropertyCardLarge.jsx";
+import Favourites from "./components/Favourites.jsx";
+
+
+function ResultsPage({results}){
+    const [favourites, setFavourites] = useState([])
+
+    const addFavourites = (property) => {
+        if(!favourites.some(fav => fav.id === property.id)){
+            setFavourites([...favourites, property])
+        }
+    }
+
+    const removeFavourites = (property) => {
+        setFavourites(favourites.filter(fav => fav.id != property.id ))
+    }
+
+    const clearAll = () => {
+        setFavourites([])
+    }
+
+    return(
+        <div className = "results-page">
+            {results.map(property => (
+            <PropertyCardLarge key={property.id} property={property} addFavourites={addFavourites}/> ))}
+            <Favourites favourites={favourites} removeFavourites={removeFavourites} clearAll = {clearAll}/>
+        </div>
+    )
+
+    
+}
+
+export default ResultsPage
---
+++ src/components/Favourites.jsx
@@ -0,0 +1,27 @@
+function Favourites({favourites,removeFavourites,clearAll}){
+    return (
+        <div className="favourite-box">
+            <h3>Favourites</h3>
+
+            {favourites.length ===0 && <p>No Favourites Yet</p> } {/*Rendering a message if favourites is empty */}
+
+            <ul className="favourite-list">
+            {favourites.map((property) => (
+                <li key={property.id} className="favourite-item" draggable="true"> 
+
+                {/*<img src = "{property.icon}" alt = "{property.name}"/> */}
+                <div className = "property-info">
+                    <strong>{property.name}</strong>
+                    <p>{property.price}</p>
+                </div>
+                <button className = "remove-button" onClick={() => removeFavourites(property.id)}>X</button>
+                </li>
+            ))}
+            </ul>
+            <button className="clear-all" onClick = {clearAll}>Clear All</button>
+                
+        </div>
+    )
+}
+
+export default Favourites
